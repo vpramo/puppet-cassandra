136f9c34fc4b437a873b80cf5e8bdf26a557d93e
msimonin <matthieu.simonin@inria.fr>
2014-10-12 23:00:55 +0200

    remove tombstone options.
    M

--- tests/site.pp
+++ tests/site.pp
@@ -1,9 +1,11 @@
 node 'seed1' {
   class { 'cassandra':
-    cluster_name      => 'testCassandra',
-    package_name      => 'cassandra',
-    seeds             => [ '192.168.100.2'],
-    listen_address    => "192.168.100.2"
+    cluster_name   => 'testCassandra',
+    package_name   => 'cassandra',
+    seeds          => [ '192.168.100.2'],
+    version        => "2.0.1",
+    listen_address => "192.168.100.2",
+    rpc_address => "192.168.100.2"
   }
 }
 
@@ -13,6 +15,8 @@ node 'node1' {
     cluster_name   => 'testCassandra',
     package_name   => 'cassandra',
     seeds          => [ '192.168.100.2'],
-    listen_address => "192.168.100.3"
+    version        => "2.0.1",
+    listen_address => "192.168.100.3",
+    rpc_address => "192.168.100.3"
   }
 }
